e4fd9dc2a13a1709a8330498e7c61ba38240692e
Zubair Bester <Zubair.bester120390@gmail.com>
2021-04-17 20:07:50 +0200

    first commit

---
+++ API/Entities/StudentEndOfTerm.cs
@@ -0,0 +1,9 @@
+namespace API.Entities
+{
+    public class StudentEndOfTerm
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string MyProperty { get; set; }
+    }
+}
