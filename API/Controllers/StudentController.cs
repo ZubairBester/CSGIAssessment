8d37272cdb1ecc0cde607b041ad2a2b7a1c6a4b8
Zubair Bester <Zubair.bester120390@gmail.com>
2021-04-19 14:29:06 +0200

    Edit

---
+++ API/Controllers/StudentController.cs
@@ -0,0 +1,83 @@
+using API.Data;
+using System;
+using System.Collections.Generic;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+   
+    public class StudentController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public StudentController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        // [HttpGet]
+        // public async Task<ActionResult<IEnumerable<GradeClass>>> GetGrades()
+        // {
+        //     return await _context.GradeStudent.ToListAsync();
+          
+        // }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentEndOfDay>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+            
+        }
+        //get all student records
+        // [HttpGet]
+        // public async Task<ActionResult<IEnumerable<StudentEndOfTerm>>> GetStudentRecords()
+        // {
+        //     return await _context.StudentRecords.ToListAsync();
+        // }
+
+        //api/grades/3s
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<GradeClass>>GetGrade(int id)
+        // {
+        //    return await _context.GradeStudent.FindAsync(id);
+        
+        // }
+        
+        //api/students/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentEndOfDay>> GetStudent(int id)
+        {
+            return await _context.Students.FindAsync(id);
+         
+        }
+         //api/students Record/3
+        // [HttpGet("{id}")]
+        // public async Task<ActionResult<StudentEndOfTerm>> GetStudentRecord(int id)
+        // {
+        //     return await _context.StudentRecords.FindAsync(id);
+         
+        // }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<StudentEndOfDay>> RegisterStudent(StudentEndOfDay studentRecord)
+        {
+            //var studentRecord = new StudentEndOfDay
+            //{
+            //    ClassName = stu,
+            //    Grade = Grade,
+            //    Date = Date,
+            //    Time = Time,
+            //    StudentsName = studentsName,
+            //    Attendency = Attendency
+            //};
+
+            _context.Students.Add(studentRecord);
+            await _context.SaveChangesAsync();
+            return studentRecord;
+        }
+    }
+}
--- API/Entities/StudentEndOfDay.cs
+++ API/Entities/StudentEndOfDay.cs
@@ -6,7 +6,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string ClassName { get; set; }
-        public string  Grade { get; set; }
+        public string Grade { get; set; }
         public DateTime Date { get; set; }
 
         public DateTime Time { get; set; }
